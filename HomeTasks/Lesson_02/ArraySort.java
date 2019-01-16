3ee5c18d195daba2d70d77c7b8d5c478131928a0
Syava <s.zabidova@yandex.ru>
2019-01-16 14:23:39 +0300

    Добавлена документация

--- HomeTasks/Lesson_02/ArraySort.java
+++ HomeTasks/Lesson_02/ArraySort.java
@@ -1,67 +1,31 @@
-import java.util.Random;
-
+/**
+ * Этот класс использует алгоритм ... сортировки.
+ * @author Sevinch
+ */
 public class ArraySort {
-    Integer[] arr = new Integer{100, 57, 98, 200, 1, 156, 900, 7};
+    private static Integer[] arr = {100, 57, 98, 200, 1, 156, 900, 7}; // массив, который нужно отсортировать
 
+    /**
+     * Метод {@code main(String[])} создаёт объект класса и выполняет сортировку.
+     *
+     * @param args аргументы командной строки
+     */
     public static void main(String[] args) {
-        ArraySort arraySort = new ArraySort();
-        /*
-
-         */
-        arraySort.sort();
+        sort();
     }
 
-    public void sort() {
+    /**
+     *Метод {@code sort()} сортирует массив по возрастанию.
+     */
+    private static void sort() {
         for (Integer i = arr.length - 1; i > 0; i--) {
             for (Integer j = 0; j < i; j++) {
-                /** Функция сравнения элементов массива попарно,если они имеют неправильный порядок,
-                то меняем местами*/
                 if (arr[j] > arr[j + 1]) {
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
                 }
             }
-        }
-
-        for (Integer i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
-        }
-    }
-}
-import java.util.Random;
-
-public class ArraySort {
-    Integer[] arr = new Integer[15];//{100, 57, 98, 200, 1, 156, 900, 7};
-
-    public static void main(String[] args) {
-        ArraySort arraySort = new ArraySort();
-        /*
-
-         */
-        Random rnd = new Random();
-        for (Integer i = 0; i < arraySort.arr.length; i++) {
-            arraySort.arr[i] = rnd.nextInt();
-            System.out.print(arraySort.arr[i] + " ");
-        }
-        arraySort.sort();
-    }
-
-    public void sort() {
-        for (Integer i = arr.length - 1; i > 0; i--) {
-            for (Integer j = 0; j < i; j++) {
-                /** Функция сравнения элементов массива попарно,если они имеют неправильный порядок,
-                то меняем местами*/
-                if (arr[j] > arr[j + 1]) {
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                }
-            }
-        }
-
-        for (Integer i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
         }
     }
 }
