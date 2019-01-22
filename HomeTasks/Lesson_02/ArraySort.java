a0a2397ece9afc0d92c6302c481d831aa0534240
Syava <s.zabidova@yandex.ru>
2019-01-22 12:05:41 +0300

    Исправление недочетов

--- HomeTasks/Lesson_02/ArraySort.java
+++ HomeTasks/Lesson_02/ArraySort.java
@@ -1,27 +1,17 @@
 /**
- * Этот класс использует алгоритм ... сортировки.
+ * Этот класс использует алгоритм пузырьковой сортировки.
  * @author Sevinch
  */
 public class ArraySort {
-    private static Integer[] arr = {100, 57, 98, 200, 1, 156, 900, 7}; // массив, который нужно отсортировать
-
-    /**
-     * Метод {@code main(String[])} создаёт объект класса и выполняет сортировку.
-     *
-     * @param args аргументы командной строки
-     */
-    public static void main(String[] args) {
-        sort();
-    }
 
     /**
      *Метод {@code sort()} сортирует массив по возрастанию.
      */
-    private static void sort() {
+    public static void sort(Integer[] arr) {
         for (Integer i = arr.length - 1; i > 0; i--) {
-            for (Integer j = 0; j < i; j++) {
+            for (int j = 0; j < i; j++) {
                 if (arr[j] > arr[j + 1]) {
-                    int temp = arr[j];
+                    Integer temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
                 }
---
+++ HomeTasks/Lesson_02/Test.java
@@ -0,0 +1,18 @@
+public class Test {
+    private static Integer[] arr = new Integer[10]; // массив, который нужно отсортировать
+
+    /**
+     * Метод {@code main(String[])} наполняет массив arr рандомно значениями и вызывает метод сортировки класса ArraySort.
+     *
+     * @param args аргументы командной строки
+     */
+    public static void main(String[] args) {
+        Random random = new Random();
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt();
+        }
+        ArraySort arraySort = new ArraySort();
+        arraySort.sort(arr);
+    }
+
+}
