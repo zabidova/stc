41c2e03759c0df6dc09c7db6869417447a1fa34c
Syava <s.zabidova@yandex.ru>
2019-01-26 22:34:46 +0300

    Добавила десериализацию

--- HomeTasks/Lesson6/Main.java
+++ HomeTasks/Lesson6/Main.java
@@ -3,13 +3,13 @@ package homeTask6;
 public class Main {
 
     public static void main(String[] args) {
-        String fileName = "D:\\my\\text_3.txt";
+        String fileName = "D:\\my\\text_3.xml";
         String name = "Saved Class";
         int id = 1;
         SavedClass savedClass = new SavedClass(name, id);
 
         Serializer serializerImpl = new SerializerImpl();
         serializerImpl.serialize(savedClass, fileName);
-        serializerImpl.deSerialize(fileName);
+        System.out.println(serializerImpl.deSerialize(fileName));
     }
 }
--- HomeTasks/Lesson6/SerializerImpl.java
+++ HomeTasks/Lesson6/SerializerImpl.java
@@ -2,6 +2,9 @@ package homeTask6;
 
 import java.io.*;
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class SerializerImpl implements Serializer {
 
@@ -9,18 +12,19 @@ public class SerializerImpl implements Serializer {
     public void serialize(Object object, String file) {
         StringBuilder stringBuilder = new StringBuilder();
         stringBuilder.append(openTag(object.getClass().getName()));
-        stringBuilder.append("\n");
+        stringBuilder.append(System.getProperty("line.separator"));
         Class c = object.getClass();
         Field[] fields = c.getDeclaredFields();
         for (Field field : fields) {
             field.setAccessible(true);
-            stringBuilder.append(openTag(field.getName() + " " + field.getType().getSimpleName()));
-            try {
+            stringBuilder.append(openTag(field.getType().getSimpleName() + " " + field.getName()));
+           try {
                 stringBuilder.append(field.get(object));
             } catch (IllegalAccessException e) {
                 e.printStackTrace();
             }
             stringBuilder.append(closeTag(field.getName()));
+           stringBuilder.append(System.getProperty("line.separator"));
         }
         stringBuilder.append(closeTag(object.getClass().getName()));
 
@@ -37,26 +41,109 @@ public class SerializerImpl implements Serializer {
     }
 
     private String closeTag (String tag) {
-        return "<" + tag + ">\n";
+        return "</" + tag + ">\n";
     }
 
     @Override
     public Object deSerialize(String file) {
-        SavedClass savedClass = null;
-        try {
-            FileInputStream fileInputStream = new FileInputStream(file);
-            ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
-            savedClass = (SavedClass) objectInputStream.readObject();
-            System.out.println(savedClass);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+        Class myClass = null;
+        Object object = null;
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))){
+            String line = reader.readLine();
+            myClass = Class.forName(getName(line));
+            object = myClass.getConstructor().newInstance();
+
+
+            while ((line = reader.readLine()) != null) {
+                String type = getName(line);
+                String value = getValue(line);
+
+                Field field = null;
+                if (!"".equals(type) && value != null) {
+
+                    field.setAccessible(true);
+
+                    switch (getType(line)) {
+                        case "byte" :
+                            field.setByte(object, Byte.parseByte(value));
+                            break;
+                        case "short" :
+                            field.setShort(object, Short.parseShort(value));
+                            break;
+                        case "int" :
+                            field.setInt(object, Integer.parseInt(value));
+                            break;
+                        case "long" :
+                            field.setLong(object, Long.parseLong(value));
+                            break;
+                        case "float" :
+                            field.setFloat(object, Float.valueOf(value));
+                            break;
+                        case "double" :
+                            field.setDouble(object, Double.parseDouble(value));
+                            break;
+                        case "boolean" :
+                            field.setBoolean(object, Boolean.parseBoolean(value));
+                            break;
+
+                    }
+                }
+            }
         } catch (IOException e) {
             e.printStackTrace();
-            System.out.println("IOException");
-        } catch (ClassNotFoundException e) {
+        } catch (InstantiationException e) {
+            System.out.println("Ошибка InstantiationException");
             e.printStackTrace();
-            System.out.println("ClassNotFoundException");
+        } catch (InvocationTargetException e) {
+            System.out.println("Ошибка InvocationTargetException");
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            System.out.println("Ошибка NoSuchMethodException");
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            System.out.println("Ошибка IllegalAccessExceptio");
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            System.out.println("Ошибка ClassNotFoundException");
+            e.printStackTrace();
         }
-        return savedClass;
+        return object;
+    }
+
+    private String getType(String line) {
+        String type = "";
+
+        Pattern pattern = Pattern.compile("(?<=\\s).+?(?=>)");
+        Matcher matcher = pattern.matcher(line);
+
+        if (matcher.find()) {
+            type = matcher.group();
+        }
+        return type;
+    }
+
+    private String getValue(String line) {
+        String value = "";
+        Pattern pattern = Pattern.compile("(?<=>).+?(?=<)");
+        Matcher matcher = pattern.matcher(line);
+
+        if (matcher.find()) {
+            value = matcher.group();
+        }
+        return value;
+    }
+
+    private String getName(String line) {
+        String name = "";
+        Pattern pattern = Pattern.compile("(?<=<)[^/].+?(?=\\s|>)");
+        Matcher matcher = pattern.matcher(line);
+
+        if (matcher.find()) {
+            name = matcher.group();
+        }
+        System.out.println("name = " + name );
+
+        return name;
     }
 }
