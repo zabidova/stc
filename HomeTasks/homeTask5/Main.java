cf6fbbcdfb3b1ea8be26ca8b1e4ddebc2f4131af
Syava <s.zabidova@yandex.ru>
2019-01-30 20:46:02 +0300

    Main

--- HomeTasks/homeTask5/Main.java
+++ HomeTasks/homeTask5/Main.java
@@ -3,7 +3,7 @@ package homeTask5;
 public class Main extends Thread {
 
     public static void main(String[] args) {
-        String[] sources = {"D:\\my\\text10.txt"};
+        String[] sources = {"text10.txt"};
         String[] words = {"Казань", "Казани", "Казанского", "Казанью"};
         String res = "C:\\Users\\Syava\\IdeaProjects\\JC14\\text4.txt";
         for (int i = 0; i < 10; i++) {
